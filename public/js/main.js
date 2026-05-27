f03db5151cc643a2da8506d8f1bc117b2c8176f8
jack <254184504+j-ckz@users.noreply.github.com>
2026-05-27 14:06:35 -0700

    OH MY FUCKING GOD I FIGURED OUT THE LOOPHOLE LETS GOOOOOOOO

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,6 @@
 function resizeIframe(obj) {
 	// THANK YOU https://stackoverflow.com/a/9976309 !!!
-	setTimeout(() => {
 		obj.style.height = obj.contentWindow.document.documentElement.scrollHeight + 2 + 'px';
-}, 250);
 }
 
 function changeFrame(page) {
