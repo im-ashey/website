64f14f3fc873fb8ee0ebe03f8b0fb247629a3b98
jack <254184504+j-ckz@users.noreply.github.com>
2026-05-27 13:32:46 -0700

    okay cool makes my navbar actually change the pages

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,10 @@
 function resizeIframe(obj) {
 	// THANK YOU https://stackoverflow.com/a/9976309 !!!
 	setTimeout(() => {
-		obj.style.height = obj.contentWindow.document.documentElement.scrollHeight + 'px';
+		obj.style.height = obj.contentWindow.document.documentElement.scrollHeight + 2 + 'px';
 }, 250);
+}
 
+function changeFrame(page) {
+	document.getElementById('page').src = page;
 }
