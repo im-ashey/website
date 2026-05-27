116c085285259faacfcef0505f45b95ea9a5eee7
ash <254184504+im-ashey@users.noreply.github.com>
2026-05-27 15:14:43 -0700

    does some fun css stuff with the nav bar to make it clear what page is selected

--- public/js/main.js
+++ public/js/main.js
@@ -3,6 +3,23 @@ function resizeIframe(obj) {
 		obj.style.height = obj.contentWindow.document.documentElement.scrollHeight + 5 + 'px';
 }
 
-function changeFrame(page) {
+function resetNav() {
+	document.getElementById('home').style.fontWeight = 'normal'
+	document.getElementById('about').style.fontWeight = 'normal'
+	document.getElementById('music').style.fontWeight = 'normal'
+	document.getElementById('webrings').style.fontWeight = 'normal'
+	document.getElementById('contact').style.fontWeight = 'normal'
+
+	document.getElementById('home').style.fontSize = '1rem'
+	document.getElementById('about').style.fontSize = '1rem'
+	document.getElementById('music').style.fontSize = '1rem'
+	document.getElementById('webrings').style.fontSize = '1rem'
+	document.getElementById('contact').style.fontSize = '1rem'
+}
+
+function changeFrame(page, button) {
 	document.getElementById('page').src = page;
+	resetNav()
+	document.getElementById(button).style.fontWeight = "bold";
+	document.getElementById(button).style.fontSize = "1.2rem";
 }
