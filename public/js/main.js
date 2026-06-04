f116412b95a416e665c9ccaf16c8694c6d2cbc25
jack <254184504+j-ckz@users.noreply.github.com>
2026-06-03 17:54:45 -0700

    adds in a landing page

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 let buttons = ["home", "about", "music", "webrings", "contact"];
 
-function quote() {
+function renderQuote() {
     fetch("../assets/other/quotes.json")
         .then((response) => response.json())
         .then((data) => {
@@ -23,6 +23,47 @@ function resizeIframe(obj) {
     obj.style.height = obj.contentWindow.document.documentElement.scrollHeight + 5 + "px";
 }
 
+function warningAccepted() {
+	localStorage.setItem('warningAccepted', 'true')
+	document.getElementById('nav').innerHTML = `
+	<ul class="nav">
+		<li><p id="home" onclick="changeFrame('pages/home.html', 'home')">home</p></li>
+		<li><p id="about" onclick="changeFrame('pages/about.html', 'about')">about</p></li>
+		<li><p id="music" onclick="changeFrame('pages/music.html', 'music')">music</p></li>
+		<li><p id="webrings" onclick="changeFrame('pages/webrings.html', 'webrings')">webrings</p></li>
+		<li><p id="contact" onclick="changeFrame('pages/contact.html', 'contact')">contact + links</p></li>
+	</ul>
+	`
+	changeFrame('pages/home.html', 'home');
+	renderQuote()
+}
+
+function checkLanding() {
+	if (localStorage.getItem('warningAccepted') == 'true') {
+		warningAccepted()
+	} else {
+		document.getElementById('landing').innerHTML = `
+		<h2>welcome to my website!</h2>
+		<p>thanks for visiting my website! just before you visit, i have a few things to warn you about.</p>
+		<ul>
+			<li><p>there is mild swearing</p></li>
+			<li><p>there might be discussion of mental health, so be warned if you're sensitive to the subject.</p></li>
+			<li><p>if it's not obvious, this website requires JS. you can audit the code <a href='//github.com/im-ashey/website'>here</a></p></li>
+			<li><p>and this website was also terribly programmed. got problems? submit a bug report on the <a href='//github.com/im-ashey/website'>github</a>.
+		</ul>
+
+		<br />
+		<p>with all that being said, do you wish to proceed?</p>
+		<br />
+
+		<ul class='nav'>
+			<li><p onclick="warningAccepted(); location.reload()">yes</p></li>
+			<li><p onclick="window.location.href = 'https://google.com'">no</p>
+		</ul>
+		`
+	}
+}
+
 function resetNav() {
     for (let i = 0; i < buttons.length; i++) {
         document.getElementById(buttons[i]).style.fontWeight = "normal";
@@ -31,8 +72,12 @@ function resetNav() {
 }
 
 function changeFrame(page, button) {
-    document.getElementById("page").src = page;
-    resetNav();
-    document.getElementById(button).style.fontWeight = "bold";
-    document.getElementById(button).style.textDecoration = "underline";
+	if (button) {
+		document.getElementById("page").src = page;
+		resetNav();
+		document.getElementById(button).style.fontWeight = "bold";
+		document.getElementById(button).style.textDecoration = "underline";
+	} else {
+		document.getElementById("page").src = page;
+	}
 }
