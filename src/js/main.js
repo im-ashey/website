0471830fc04c580bd6c442c29404445f1ed2bfab
ash <254184504+im-ashey@users.noreply.github.com>
2026-06-07 17:14:14 -0700

    adds in my goatcounter

--- src/js/main.js
+++ src/js/main.js
@@ -12,6 +12,7 @@ const cssRoot = document.querySelector(":root");
 function renderFunStuff() {
     checkTheme();
     renderQuote();
+    renderCounts();
     renderStatus();
     renderLastFM();
     renderChangelog();
@@ -88,6 +89,27 @@ function renderQuote() {
         .catch((error) => console.error("something broke idk", error));
 }
 
+function renderCounts() {
+    // 3ob0irevb5i4tq3xc5goz8f3v456svdmqen71pz8hg28urzq1
+    // https://stats.ashey.me/api/v0/stats/total
+
+    fetch('https://stats.ashey.me/api/v0/stats/total', {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer 3ob0irevb5i4tq3xc5goz8f3v456svdmqen71pz8hg28urzq1",
+    }
+    })
+    .then((response) => response.json())
+    .then ((data) => {
+        const counterDOM = document.querySelector('p#count')
+        const hitCount = data['total']
+
+        counterDOM.innerHTML = `this website has been visited ${hitCount} times`
+    })
+
+}
+
 // thanks max
 function renderStatus() {
     (async () => {
@@ -143,7 +165,6 @@ function renderChangelog() {
     fetch("https://api.github.com/repos/im-ashey/website/commits")
         .then((response) => response.json())
         .then((data) => {
-            console.log(data);
             for (let commitNum = 0; commitNum < 10; commitNum++) {
                 const newHtml = `
                 <p>Author: ${data[commitNum]["commit"]["author"]["name"]}</p>
