bd2d8d1fcb8843228782f46f563623596f701eb5
jack <254184504+j-ckz@users.noreply.github.com>
2026-06-11 13:33:36 -0700

    spreads my website wayyyy out

---
+++ src/js/about.js
@@ -0,0 +1,26 @@
+const username = "ash"; // change the username!!!
+const posts_url = "https://cafe.frizzbees.dev/get_posts/1?name=";
+const profile_url = "https://social.nekoweb.org/profile/?view=";
+const post_url = "https://social.nekoweb.org/post/?id=";
+
+(async () => {
+    try {
+        const request = await fetch(posts_url + username);
+        let json = await request.json();
+        json = json[0];
+
+        const timestamp = json["timestamp"] * 1000;
+        const time = new Date(timestamp).toUTCString();
+
+        const div = document.getElementById("nekocafe-status");
+
+        if (div) {
+            div.innerHTML = `
+                <p id="nekocafe-text"><a href="${post_url + json["id"]}">${json["post"]}</a></p>
+                <p id="nekocafe-time">${time}</p>
+            `; // make sure the height on the img fits your page!!!
+        }
+    } catch (error) {
+        console.error(error);
+    }
+})();
