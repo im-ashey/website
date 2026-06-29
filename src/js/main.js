4114005b7b75325bc972f70cf77341522e664519
jack <254184504+j-ckz@users.noreply.github.com>
2026-06-29 10:49:30 -0700

    some more web maintence

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,5 @@
 function renderFunStuff() {
     renderQuote();
-    renderCounts();
 }
 
 function renderQuote() {
@@ -24,26 +23,4 @@ function renderQuote() {
             }
         })
         .catch((error) => console.error("something broke idk", error));
-}
-
-function renderCounts() {
-    // 3ob0irevb5i4tq3xc5goz8f3v456svdmqen71pz8hg28urzq1
-    // https://stats.ashey.me/api/v0/stats/total
-
-    fetch('https://stats.ashey.me/api/v0/stats/total', {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: "Bearer 3ob0irevb5i4tq3xc5goz8f3v456svdmqen71pz8hg28urzq1",
-    }
-    })
-    .then((response) => response.json())
-    .then ((data) => {
-        console.log(data)
-        const counterDOM = document.querySelector('p#count')
-        const hitCount = data['total']
-
-        counterDOM.innerHTML = `this website has been visited ${hitCount} times`
-    })
-
 }
