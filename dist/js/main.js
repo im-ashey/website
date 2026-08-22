d554da1f5b1e06e0225b69254f4230f43ffcecf9
jack <254184504+j-ckz@users.noreply.github.com>
2026-08-21 19:00:32 -0700

    starts a TS project

---
+++ dist/js/main.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let titleDOM = document.querySelector('title-header#h1');
+if (titleDOM) {
+    titleDOM.innerHTML = window.location.href;
+}
+//# sourceMappingURL=main.js.map
---
+++ src/ts/main.ts
@@ -0,0 +1,5 @@
+let titleDOM = document.querySelector('title-header#h1')
+
+if (titleDOM) {
+	titleDOM.innerHTML = window.location.href
+}
