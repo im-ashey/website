98243a4dd9ac6f2ed919fafd62ca9f7f46b3964f
jack <254184504+j-ckz@users.noreply.github.com>
2026-08-29 21:32:20 -0700

    removes the ending / from the title

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -3,6 +3,7 @@ import { setSavedTheme, setDarkTheme, setLightTheme } from './theme.js'
 
 let darkThemeButtonDOM = document.getElementById('darkThemeButton') as HTMLAnchorElement;
 let lightThemeButtonDOM = document.getElementById('lightThemeButton') as HTMLAnchorElement;
+const endDigit = -2;
 
 window.addEventListener("DOMContentLoaded", () => {
     setSavedTheme();
@@ -19,12 +20,12 @@ function renderHeader() {
     const windowLocation = window.location.href;
 
     if (windowLocation?.includes('https')) {
-        const headerText = windowLocation.slice(5, -1);
+        const headerText = windowLocation.slice(5, endDigit);
         headerDOM.innerHTML = headerText;
         titleDOM.innerHTML = headerText;
 
     } else {
-        const headerText = windowLocation.slice(4, -1);
+        const headerText = windowLocation.slice(4, endDigit);
         headerDOM.innerHTML = headerText;
         titleDOM.innerHTML = headerText;
     }
