190e66fe7c1dab0dcd8bf8da4f46443b45a38e21
jack <254184504+j-ckz@users.noreply.github.com>
2026-08-29 20:25:19 -0700

    fixes a structuring bug.

--- src/ts/navigation.ts
+++ src/ts/navigation.ts
@@ -1,10 +1,10 @@
 const links = ['about', 'music', 'contact', 'other'];
 const linksLength = links.length - 1;
 let navDOM = document.getElementById('navigationElement') as HTMLUListElement;
+let navButtonElement = document.createElement('ul') as HTMLElement;
 
 function generateNav() {
-    for (let i = 0 as number; i <= linksLength; i++) {
-        let navButtonElement = document.createElement('ul') as HTMLElement;
+    for (let i = 0; i <= linksLength; i++) {
         let navButtonListElement = document.createElement('li');
         let navButtonAnchorElement = document.createElement('a');
         let linkTextNode = document.createTextNode(links[i]!);
@@ -13,9 +13,9 @@ function generateNav() {
         navButtonAnchorElement.appendChild(linkTextNode);
         navButtonAnchorElement.href = links[i]!;
         navButtonListElement.appendChild(navButtonAnchorElement);
-
-        navDOM.appendChild(navButtonElement);
     }
+    navDOM.appendChild(navButtonElement);
+
 }
 
 export { generateNav }
